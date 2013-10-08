5f4670c839db72c75ed30214a9ad9b7243da3d57
mika vanhaviitakoski <mika@vanhaviitakoski.com>
2013-10-08 22:34:58 +0300

    Added Program.php
    Added Program.php file to repo.

---
+++ Program.php
@@ -0,0 +1,24 @@
+<?
+class Program{
+
+	var $id = 0;
+	var $name = "";
+	var $originalName = "";
+	var $seriesTitle = "";
+	var $description = "";
+	var $startTime;
+	var $endTime;
+
+	var $seasonNo = 0;
+	var $productionYear = 0;
+	var $productionCountry = "";
+	var $director = "";
+	var $movie = false;
+	var $sport = false;
+	var $series = false;
+	var $children = false;
+	var $ageRating = "";
+
+}
+
+?>
